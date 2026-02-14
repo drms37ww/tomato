e504748ee9ea74a3721ef706bb8212b41fa803ca
drms37ww <sena37ww@gmail.com>
2026-02-14 21:40:58 +0900

    Update sw.js

--- sw.js
+++ sw.js
@@ -2,21 +2,28 @@ const CACHE = 'tomato-v2';
 const SHELL = ['index.html', 'manifest.json', 'icon-192.png', 'icon-512.png'];
 
 self.addEventListener('install', e => {
+  self.skipWaiting(); // activate immediately, don't wait
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(SHELL)));
-  self.skipWaiting();
 });
 
 self.addEventListener('activate', e => {
-  e.waitUntil(caches.keys().then(ks => Promise.all(
-    ks.filter(k => k !== CACHE).map(k => caches.delete(k))
-  )));
-  self.clients.claim();
+  e.waitUntil(
+    caches.keys().then(ks => Promise.all(
+      ks.filter(k => k !== CACHE).map(k => caches.delete(k))
+    )).then(() => self.clients.claim()) // take control of all pages immediately
+  );
 });
 
 self.addEventListener('fetch', e => {
   const url = new URL(e.request.url);
-  // Only cache shell files, let proxy requests pass through
   if (url.origin === location.origin) {
-    e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
+    // Network-first for shell files so updates propagate instantly
+    e.respondWith(
+      fetch(e.request).then(r => {
+        const clone = r.clone();
+        caches.open(CACHE).then(c => c.put(e.request, clone));
+        return r;
+      }).catch(() => caches.match(e.request))
+    );
   }
 });
