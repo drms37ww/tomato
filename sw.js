06e22409cd073db4e264811ca7caae6c2eafc17c
drms37ww <sena37ww@gmail.com>
2026-02-14 21:45:53 +0900

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,8 +1,8 @@
-const CACHE = 'tomato-v2';
+const CACHE = 'tomato-v3';
 const SHELL = ['index.html', 'manifest.json', 'icon-192.png', 'icon-512.png'];
 
 self.addEventListener('install', e => {
-  self.skipWaiting(); // activate immediately, don't wait
+  self.skipWaiting();
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(SHELL)));
 });
 
@@ -10,14 +10,13 @@ self.addEventListener('activate', e => {
   e.waitUntil(
     caches.keys().then(ks => Promise.all(
       ks.filter(k => k !== CACHE).map(k => caches.delete(k))
-    )).then(() => self.clients.claim()) // take control of all pages immediately
+    )).then(() => self.clients.claim())
   );
 });
 
 self.addEventListener('fetch', e => {
   const url = new URL(e.request.url);
   if (url.origin === location.origin) {
-    // Network-first for shell files so updates propagate instantly
     e.respondWith(
       fetch(e.request).then(r => {
         const clone = r.clone();
