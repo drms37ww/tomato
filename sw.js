6798aadf50164bf645653d9054cedd5cdcce3b96
drms37ww <sena37ww@gmail.com>
2026-02-14 21:29:48 +0900

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,22 @@
+const CACHE = 'tomato-v1';
+const SHELL = ['index.html', 'manifest.json', 'icon-192.png', 'icon-512.png'];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(SHELL)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(caches.keys().then(ks => Promise.all(
+    ks.filter(k => k !== CACHE).map(k => caches.delete(k))
+  )));
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', e => {
+  const url = new URL(e.request.url);
+  // Only cache shell files, let proxy requests pass through
+  if (url.origin === location.origin) {
+    e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
+  }
+});
